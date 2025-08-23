3bde1182c568b1e4ad121ef85bbd57197262bd79
Himanshu-939 <himanshu92verma92@gmail.com>
2025-08-23 22:26:35 +0530

    work10

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,14 @@
+import Hero from '@/components/Hero'
 import React from 'react'
-import { Button } from '../components/ui/button'
-import Hero from '../components/Hero'
+import RecentBlog from '@/components/RecentBlog'
+import PopularAuthors from '@/components/PopularAuthors'
 
 const Home = () => {
   return (
     <div className='pt-20'>
       <Hero/>
+      <RecentBlog/>
+      <PopularAuthors/>
     </div>
   )
 }
