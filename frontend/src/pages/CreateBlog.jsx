3f39fdcefc6195da34fdc3ffe3e7e68c9f865d3d
Himanshu-939 <himanshu92verma92@gmail.com>
2025-08-24 01:39:06 +0530

    work12

--- frontend/src/pages/CreateBlog.jsx
+++ frontend/src/pages/CreateBlog.jsx
@@ -1,6 +1,6 @@
 import { Button } from '../components/ui/button'
 import { Card } from '../components/ui/card'
-import { Input } from './components/ui/input'
+import { Input } from '../components/ui/input'
 import { Label } from '../components/ui/label'
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from '../components/ui/select'
 import { setBlog } from '../redux/blogSlice'
