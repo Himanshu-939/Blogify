741805bc59a6df83042d4ec0d749a610e9103903
Himanshu-939 <himanshu8verma08@gmail.com>
2025-08-23 22:25:31 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,27 +3,58 @@ import Signup from './pages/Signup'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
-import Bloggs from './pages/Bloggs'
-import About from './pages/About'
 import Navbar from './components/Navbar'
+import Profile from './pages/Profile'
+import Blog from './pages/Blog'
+import CreateBlog from './pages/CreateBlog'
 import Dashboard from './pages/Dashboard'
-
+import YourBlog from './pages/YourBlog'
+import BlogView from './pages/BlogView'
+import Footer from './components/Footer'
+import About from './pages/About'
+import Comments from './pages/Comments'
+import UpdateBlog from './pages/UpdateBlog'
+import ProtectedRoute from './components/ProtectedRoute'
+import SearchList from './pages/SearchList'
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element:<><Navbar/><Home/></>
+    path: "/",
+    element: <><Navbar/><Home /><Footer/></>
   },
   {
-    path:"/blogs",
-    element:<><Navbar/><Bloggs/></>
+    path: "/blogs",
+    element: <><Navbar/><Blog /><Footer/></>
   },
   {
-    path:"/about",
-    element:<><Navbar/><About/></>
-
+    path: "/about",
+    element: <><Navbar/><About /><Footer/></>
   },
-
+  {
+    path: "/search",
+    element: <><Navbar/><SearchList/><Footer/></>
+  },
+  {
+    path: "/blogs/:blogId",
+    element: <><Navbar/><ProtectedRoute><BlogView /></ProtectedRoute></>
+  },
+  {
+    path: "/write-blog",
+    element: <><Navbar/><CreateBlog /></>
+  },
+ 
+  {
+    path: "/profile",
+    element: <><Navbar/><Profile /></>
+  },
+  // {
+  //   path: "write-blog/:blogId",
+  //       element: <><Navbar/><CreateBlog /></>
+  // },
+  // {
+  //   path: "/dashboard",
+  //   element: <><Navbar/><Dashboard /></>
+  // },
   {
     path:"/dashboard",
     element: <><Navbar/><ProtectedRoute><Dashboard/></ProtectedRoute></>,
@@ -52,17 +83,15 @@ const router = createBrowserRouter([
       
     ]
    },
-
   {
-    path:"/login",
-    element:<><Navbar/><Login/></>
+    path: "/signup",
+    element: <><Navbar/><Signup /></> 
   },
   {
-    path:"/signup",
-    element:<><Navbar/><Signup/></>
+    path: "/login",
+    element: <><Navbar/><Login /></>
   },
 ])
-
 
 const App = () => {
   return (
